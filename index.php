96c9e39fefbce5bc1c14a81f7fbb3334c612a4ce
111cloudstrife <111cloudstrife@gmail.com>
2018-01-03 15:53:27 +0200

    Main index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php' ?>
+
+<section id="top">
+<div class="container">
+<div class="row">
+	<h1>Арендуйте себе<br />автомобиль</h1>
+	<h2>Потратье 2 минути и получите <br />лучшее предложение среди автовладельцев</h2>
+	<div class="col-md-6 npd">
+	<input type="text" placeholder="Пхукет"/>
+	<button class="gray">
+		
+		<h4><a href="javascript:void(0);">НАЙТИ АВТОМОБИЛЬ</a></h4>
+	</button>
+	<img src="img/search.png" alt="">
+	<h5>уже 1,765 автовладельцев готовы сдать свой автомобиль</h5>
+	</div>
+</div>
+</div>
+</section>
+
+<?php include 'footer.php' ?>
