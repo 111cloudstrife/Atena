97851824bb760bbdacd0cd46ee2c2f18b27f726f
111cloudstrife <111cloudstrife@gmail.com>
2018-01-03 15:59:41 +0200

    Update offers page

---
+++ offers.php
@@ -0,0 +1,79 @@
+<?php include 'header_logged.php' ?>
+
+
+<section id="my_dv">
+<div class="container">
+<div class="row">
+	<h2>Новые заказы</h2>
+	<h3>Обрабатывайте быстро новые заказы</h3>
+	<div class="dv offer col-md-4 col-sm-6">
+		<img src="img/ava2.png" alt="">
+		<h1>Света</h1>
+		<h3>Пхукет</h3>
+		<h2>
+			С 29.11.17 10:00 до 12.12.17 10:00
+			<br>Бюджет: по договоренности
+			<br>Тип авто: Экономный вариант
+			<br>Доставка: Патонг, The Htel Phuket
+		</h2>
+		<button class="aqua">Принять</button>
+		<button class="white">Отклонить</button>
+	</div>
+	<div class="dv offer col-md-4 col-sm-6">
+		<img src="img/ava2.png" alt="">
+		<h1>Света</h1>
+		<h3>Пхукет</h3>
+		<h2>
+			С 29.11.17 10:00 до 12.12.17 10:00
+			<br>Бюджет: по договоренности
+			<br>Тип авто: Экономный вариант
+			<br>Доставка: Патонг, The Htel Phuket
+		</h2>
+		<button class="aqua">Принять</button>
+		<button class="white">Отклонить</button>
+	</div>
+	<div class="dv offer col-md-4 col-sm-6">
+		<img src="img/ava2.png" alt="">
+		<h1>Света</h1>
+		<h3>Пхукет</h3>
+		<h2>
+			С 29.11.17 10:00 до 12.12.17 10:00
+			<br>Бюджет: по договоренности
+			<br>Тип авто: Экономный вариант
+			<br>Доставка: Патонг, The Htel Phuket
+		</h2>
+		<button class="aqua">Принять</button>
+		<button class="white">Отклонить</button>
+	</div>
+	<div class="dv offer col-md-4 col-sm-6">
+		<img src="img/ava2.png" alt="">
+		<h1>Света</h1>
+		<h3>Пхукет</h3>
+		<h2>
+			С 29.11.17 10:00 до 12.12.17 10:00
+			<br>Бюджет: по договоренности
+			<br>Тип авто: Экономный вариант
+			<br>Доставка: Патонг, The Htel Phuket
+		</h2>
+		<button class="aqua">Принять</button>
+		<button class="white">Отклонить</button>
+	</div>
+	<div class="dv offer col-md-4 col-sm-6">
+		<img src="img/ava2.png" alt="">
+		<h1>Света</h1>
+		<h3>Пхукет</h3>
+		<h2>
+			С 29.11.17 10:00 до 12.12.17 10:00
+			<br>Бюджет: по договоренности
+			<br>Тип авто: Экономный вариант
+			<br>Доставка: Патонг, The Htel Phuket
+		</h2>
+		<button class="aqua">Принять</button>
+		<button class="white">Отклонить</button>
+	</div>
+	
+</div>
+</div>
+</section>
+
+<?php include 'footer.php' ?>
