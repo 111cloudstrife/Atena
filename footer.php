5de2074eb3904bade1c3448584cba41c62ab6972
111cloudstrife <111cloudstrife@gmail.com>
2018-01-03 15:45:06 +0200

    Auth

---
+++ footer.php
@@ -0,0 +1,32 @@
+<section id="footer">
+<div class="container">
+<div class="row" style="margin:0 -30px;">
+	<div class="col-md-4 col-sm-6">
+		<a href="javascript:void(0);">Политика конфиденциальности</a>
+	</div>
+	
+	<div class="col-md-2 col-sm-3 col-sm-offset-3 col-md-offset-6 text-center dropup">
+		<h6 type="button" data-toggle="dropdown">ЯЗЫК: <span>РУССКИЙ</span><span class="caret" style="color:#d7d7d7;top: -3px; left: 5px;"></span></h6>
+		<ul class="dropdown-menu dropup">
+			<li class="selected"><a href="#">РУССКИЙ</a></li>
+			<li><a href="#">ENGLISH</a></li>
+		</ul>
+	</div>
+</div>
+<div class="row" style="margin:30px -30px;">
+	<div class="col-md-4">
+		<h5>© 2017 <span>Athena</span> - Все права защищены.</h5>
+	</div>
+	<div class="col-md-2 col-md-offset-6 text-center">
+		<div class="social">
+			<div><img src="img/fb.png" alt=""></div>
+			<div><img src="img/vk.png" alt=""></div>
+			<div><img src="img/insta.png" alt=""></div>
+		</div>
+	</div>
+</div>
+</div>
+</section>
+
+</body>
+</html>
